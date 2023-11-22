0a5f2deaa913e21c299cbed3631154797ef700da
FabioVasi <vasifabio96@gmail.com>
2023-11-22 21:01:41 +0100

    fix controller

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewLeadEmail;
+use App\Mail\NewLeadEmailMd;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
-    //
+    function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'message' => 'required',
+        ]);
+
+        // redirect if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $lead = Lead::create($request->all());
+
+        Mail::to('info@boolpress.com')->send(new NewLeadEmail($lead));
+
+        Mail::to($lead->email)->send(new NewLeadEmailMd($lead));
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Form sent successfully 👍'
+            ]
+        );
+    }
 }
